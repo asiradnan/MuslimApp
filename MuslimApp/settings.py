1ae3ab91e044e9b5eeddbbad826c9408ce59d1f0
asiradnan <asiradnan23@gmail.com>
2024-08-16 13:32:13 +0600

    To host

--- MuslimApp/settings.py
+++ MuslimApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3asl3bhw2w55i780$@$3z-f_2d#6ckab2pex@pmv91me6p_zzl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
