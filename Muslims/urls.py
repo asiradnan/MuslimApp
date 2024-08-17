893d84174bbe56c5130d4c1579accfe3443d6f9c
asiradnan <asiradnan23@gmail.com>
2024-08-17 17:20:10 +0600

    jwt settings

--- Muslims/urls.py
+++ Muslims/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("",views.list,name="list"),
@@ -7,4 +8,5 @@ urlpatterns = [
     path("login",views.login,name="login"),
     path("logout",views.log_out,name="logout"),
     path("loggedin",views.user_detail,name="user_detail"),
+    path('refreshtoken', TokenRefreshView.as_view(), name='token_refresh'),
 ]
