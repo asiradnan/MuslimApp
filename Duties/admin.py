5c53b4e7a56e51f2bdb345d6f2af5c7277352549
asiradnan <verifywithasir@gmail.com>
2024-09-07 15:42:32 +0600

    From VPS

--- Duties/admin.py
+++ Duties/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from . models import Task, CheckList, Feedback, OldTaskCheckList, References, Quran_References
 
 admin.site.register(Task)
-admin.site.register(CheckList)
 admin.site.register(Feedback)
-admin.site.register(OldTaskCheckList)
 admin.site.register(References)
 admin.site.register(Quran_References)
